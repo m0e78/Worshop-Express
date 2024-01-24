e111ab0d9361885a4b406c34c0c81e234812c34f
Moez <moezzanza@gmail.com>
2024-01-24 15:44:31 +0100

    auth

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-const express=require('express')
-const app=express()
-const port=5000
+const express = require("express")
+const app = express()
+const port = 5000
+const bodyparser = require("body-parser")
+const user = {
+  email: "email@gmail.com",
+  pass: "12345",
+}
+const home = require("./routes/home")
+const about = require("./routes/about")
 
-const home=require('./routes/home')
-const about=require('./routes/about')
-
-app.use('/home',home)
-app.use('/about',about)
-
-
-app.listen(port,(req,res)=>{
-    console.log(`http://localhost:${port}`)
+app.use(bodyparser.json())
+// app.use(bodyparser.urlencoded({extended:false}))
+const auth = (req, res, next) => {
+  const { email, pass } = req.body
+  console.log(req.body)
+  if (email == user.email && pass == user.pass) {
+    next()
+  } else {
+    res.status(401).send("incorrect identification")
+  }
+}
+app.use("/home", auth)
+app.use("/home", home)
+app.use("/about", about)
+app.listen(port, (req, res) => {
+  console.log(`http://localhost:${port}`)
 })
